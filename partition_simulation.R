d6d33ce1525762dcfe39974fd05e9132d39e9c42
echuu <chuu3ric@gmail.com>
2019-10-30 15:57:53 -0500

    lenk sim

--- partition_simulation.R
+++ partition_simulation.R
@@ -52,6 +52,43 @@ rpart.plot(reg.tree, type = 4)
 reg.tree$splits
 
 
+# -----------------------------------------------------------------------------
+
+# lenk paper simulation 
+
+mu = 30
+sigma_sq = 4
+m_0 = 0
+w_0 = 0.05
+r_0 = 3
+s_0 = 3
+
+N = 50
+
+# generate 50 samples from N(mu, sigma_sq)
+y = rnorm(N, mu, sqrt(sigma_sq))
+
+ybar = mean(y)
+
+# compute posterior parameters
+m_n = (N * ybar + w_0 * m_0) / (N + w_0)
+w_n = w_0 + N
+r_n = r_0 + N
+s_n = s_0 + sum((y - ybar)^2) + (N * w_0 / (N + w_0)) * (ybar - m_0)^2
+
+p_y = pi^(-N / 2) * (w_0 / w_n)^(1/2) * gamma(r_n / 2) / gamma(r_0 / 2) * 
+    s_0^(r_0 / 2) / s_n^(r_n / 2)
+
+LIL = log(p_y) # -118.332 (paper says -117, but difference arises from RNG)
+
+
+
+# generate samples from the posterior probability to form the HME estimator
+
+
+
+
+
 
 
 
