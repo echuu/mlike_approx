fddb18e670f1be1b3f5cd9181adf598d45750f91
echuu <chuu3ric@gmail.com>
2019-11-03 00:35:37 -0500

    normal-normal plots

--- NIG_partition.R
+++ NIG_partition.R
@@ -61,17 +61,18 @@ LIL = log(p_y) # -118.332 (paper says -117, but difference arises from RNG)
 # s_0      : (1 x 1) prior scale
 psi_p = function(y, mu, sigma_sq, m_0, w_0, r_0, s_0) {
     
-    # for the vector y, evaluate the likelihood for j-th set of parameter values
-    lik = numeric(length(mu))
+    # for  y_1:N, evaluate the likelihood for j-th set of parameter values
+    loglik = numeric(length(mu))
     
     for (j in 1:length(mu)) {
-        lik[j] = sum(dnorm(y, mu[j], sqrt(sigma_sq[j]), log = TRUE))
+        loglik[j] = sum(dnorm(y, mu[j], sqrt(sigma_sq[j]), log = TRUE))
     }
     
-    p_mu = dnorm(mu, m_0, sqrt(sigma_sq / w_0), log = TRUE)
-    p_sigma_sq = dinvgamma(sigma_sq, r_0 / 2, s_0 / 2)
+    # following two computations are unused for now
+    log_p_mu = dnorm(mu, m_0, sqrt(sigma_sq / w_0), log = TRUE)
+    log_p_sigma_sq = dinvgamma(sigma_sq, r_0 / 2, s_0 / 2)
     
-    return(lik) 
+    return(loglik) 
 }
 
 J = 1000 # number of random draws used per estimate
@@ -83,7 +84,6 @@ mu_post = rnorm(J, m_n, sqrt(sigma_sq / w_n)) # (D x 1)
 sigma_sq_post = rinvgamma(J, shape = r_n / 2, scale = s_n / 2)
 
 # (2) for each u drawn from the posterior, evaluate psi(u) = psi(mu, sigma_sq)
-# ---- this is for sure not doing what i'm expecting it to do
 psi_u = psi_p(y, mu_post, sigma_sq_post, m_0, w_0, r_0, s_0) # (J x 1)
 u_df = data.frame(mu = mu_post, sigsq = sigma_sq_post, psi_u = psi_u) # (J x 3)
 
@@ -95,24 +95,10 @@ partition.tree(u_tree, cex = 1)
 
 # ------------------------------------------------------------------------------
 
-J = 1000 # number of random draws used per estimate
-
-# (0) sample from mu | sigma_sq, y
-mu_post = rnorm(J, m_n, sqrt(sigma_sq / w_n)) # (D x 1)
-
-# (1) sample from sigma_sq | y
-sigma_sq_post = rinvgamma(J, shape = r_n / 2, scale = s_n / 2)
-
-# (2) for each u drawn from the posterior, evaluate psi(u) = psi(mu, sigma_sq)
-# ---- this is for sure not doing what i'm expecting it to do
-psi_u = psi(y, mu_post, sigma_sq_post, m_0, w_0, r_0, s_0)$psi_u # (J x 1)
-u_df = data.frame(mu = mu_post, sigsq = sigma_sq_post, psi_u = psi_u) # (J x 3)
-
 hist(mu_post)
 hist(sigma_sq_post)
 
-
-par(mfrow = c(3, 1))
+par(mfrow = c(1, 3))
 
 
 
--- plot_partition.R
+++ plot_partition.R
@@ -8,7 +8,7 @@ library('mvtnorm')   # multivariate normal density
 
 set.seed(123)
 
-N = 50
+N = 1000
 
 # y | mu ~ N (mu, Sigma)
 mu = c(3, 1)
@@ -29,40 +29,49 @@ ybar = apply(y, 2, mean) # mean of each component
 Sigma_N = solve(solve(Sigma_0) + N * solve(Sigma))
 mu_N = Sigma_N %*% (solve(Sigma_0) %*% mu_0 + N * solve(Sigma) %*% ybar)
 
+# define function psi()
+psi = function(y, mu, Sigma, mu_0, Sigma_0) {
+    
+    # for the vectors y, evaluate the log-like for j-th set of parameter values
+    loglik = numeric(nrow(mu))
+    
+    # likelihood needs to be computed for each of the parameter settings 
+    for (j in 1:nrow(mu)) {
+        loglik[j] = sum(dmvnorm(y, mu[j,], Sigma, log = TRUE))
+    }
+    
+    # log prior of mu -- unused for now
+    log_p_mu = dmvnorm(mu, mu_0, Sigma_0, log = TRUE)
+    
+    return(loglik)
+}
+
 # generate samples from the posterior probability to form the HME estimator
 J = 1000 # number of random draws used per estimate
 
-# (0) sample from mu | sigma_sq, y
+# (0) sample from mu | y
 mu_post = mvrnorm(J, mu_N, Sigma_N) # (D x 1)
 
 # plot the points drawn from the posterior distribution
 mu_df = data.frame(mu1 = mu_post[,1], mu2 = mu_post[,2])
-ggplot(mu_df, aes(mu1, mu2)) + geom_point() + theme_bw()
+ggplot(mu_df, aes(mu2, mu1)) + geom_point() + theme_bw()
 
+# (1) evaluate psi(u) = psi(mu)
+psi_u = psi(y, mu_post, Sigma, mu_0, Sigma_0)
+u_df = data.frame(mu1 = mu_post[,1], mu2 = mu_post[,2], psi_u = psi_u) # (J x 3)
 
-# define function psi()
-psi = function(y, mu, Sigma, mu_0, Sigma_0) {
-    log_like = dmvnorm(y, mu, Sigma) # evaluate y row-wise
-    
-    # likelihood needs to be computed for each of the parameter settings 
-    
-    p_mu = dmvnorm(mu, mu_0, Sigma_0)
-    
-    return(list(psi_u = log(like * p_mu * p_sigma_sq),
-                p_mu = p_mu, p_sigma_sq = p_sigma_sq))
-}
+# (2) fit decision tree
+u_tree = tree(psi_u ~ mu1 + mu2, u_df)
 
-
-# TODO -------------------------------------------------------------------------
-
-# (3) fit decision tree
-u_tree = tree(psi_u ~ mu + sigsq, u_df)
-
-# (4) plot the partition over the parameter space
+# (3) plot the partition over the parameter space
 partition.tree(u_tree, cex = 1)
 
-# (5) look at posterior distributions of mu, sigma_sq to see if there are more
-#     partitions in areas of higher posterior probability
+
+# overlay partition on scatterplot of the posterior distribution
+plot(mu_df[,2], mu_df[,1], pch = 20, cex = 0.5, col = "blue",
+     xlab = 'mu2', ylab = 'mu1', main = 'n = 100')
+partition.tree(u_tree, add = TRUE, cex = 0.01)
+
 
 
 
