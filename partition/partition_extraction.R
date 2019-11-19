9faa888e50515d30c630605127c5ee054ff07197
echuu <chuu3ric@gmail.com>
2019-11-19 14:54:22 -0600

    partition extract

---
+++ partition/partition_extraction.R
@@ -0,0 +1,23 @@
+
+
+# partition_extraction.R
+
+# goal: extract the partition from the fitted regression tree output
+# misc: using the partitions, compute the midpoint of all the partitions
+
+
+# get decision tree rule/path pattern for every row of predicted dataset
+# https://tinyurl.com/t5mgkce
+
+
+# what we want should be a function of this solution here..
+# list of splitting conditions for each terminal node
+# https://tinyurl.com/tpuvaeb
+
+
+
+
+
+
+
+
