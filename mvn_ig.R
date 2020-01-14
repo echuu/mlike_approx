e11225396afb5cc22c98beee1a70eef0d15cb394
echuu <chuu3ric@gmail.com>
2020-01-14 15:18:58 -0600

    d = 3 : working

--- mvn_ig.R
+++ mvn_ig.R
@@ -3,7 +3,7 @@
 library(mvtnorm)           # for draws from multivariate normal
 library("numDeriv")        # for grad() function - numerical differentiation
 library('MCMCpack')        # for rinvgamma() function
-
+library('microbenchmark')
 
 # path for lenovo
 # setwd("C:/Users/ericc/mlike_approx")
@@ -53,6 +53,12 @@ b_n     =  b_0 + 0.5 * (t(y) %*% y +                          # (1 x 1)
                             t(mu_beta) %*% V_beta_inv %*% mu_beta - 
                             t(mu_star) %*% V_star_inv %*% mu_star) %>%  c()
 
+# store posterior parameters
+post = list(V_star  =  V_star,
+            mu_star =  mu_star,
+            a_n     =  a_n,
+            b_n     =  b_n)
+
 # sanity check: posterior mean of beta, sigmasq (values make sense!)
 mu_star
 b_n / (a_n - 1)
@@ -95,12 +101,6 @@ for (j in 1:J) {
 u_post = data.frame(beta_post = beta_post, sigmasq_post = sigmasq_post)
 
 
-# store posterior parameters
-post = list(V_star  =  V_star,
-            mu_star =  mu_star,
-            a_n     =  a_n,
-            b_n     =  b_n)
-
 
 # ------------------------------------------------------------------------------
 
@@ -108,13 +108,13 @@ post = list(V_star  =  V_star,
 
 # START TEST : move this testing code elsewhere later 
 
-(u_post_test = head(u_post))
+# (u_post_test = head(u_post))
 
 # test for vector input
-psi_true_mvn(u_post_test[2,], post = post)
+# psi_true_mvn(u_post_test[2,], post = post)
 
 # test for matrix input, using apply()
-apply(u_post_test, 1, psi_true_mvn, post = post) %>% unname() # (J x 1)
+# apply(u_post_test, 1, psi_true_mvn, post = post) %>% unname() # (J x 1)
 
 ## END TEST -------
 
@@ -123,7 +123,7 @@ apply(u_post_test, 1, psi_true_mvn, post = post) %>% unname() # (J x 1)
 
 psi_u = apply(u_post, 1, psi_true_mvn, post = post) %>% unname() # (J x 1)
 
-head(psi_u)
+# head(psi_u)
 
 
 # (1.2) construct u_df -- this will require some automation for colnames
@@ -162,17 +162,14 @@ u_rpart = rpart(psi_u ~ ., u_df)
 # psi_mvn() used in main algo
 
 # test for vector input
-psi_mvn(u_post_test[1,], prior)
+# psi_mvn(u_post_test[1,], prior)
 
 # test for matrix input, using apply()
-apply(u_post_test, 1, psi_mvn, prior = prior) %>% unname() # (J x 1)
+# apply(u_post_test, 1, psi_mvn, prior = prior) %>% unname() # (J x 1)
 
 ## END TEST -------
 
 # ------------------------------------------------------------------------------
-
-
-## TODO: repliate the main algorithm here
 
 ## (3) process the fitted tree
 
@@ -202,12 +199,12 @@ head(param_out)
 
 # ------------------------------------------------------------------------------
 
-## TODO : perform algorithm for a batch approximation (averaged)
+## DONE 1/14 : define, validate lambda() --> mvn_ig_helper.R
+## DONE 1/14 : test/validate lambda (closed form vs. numerical) 
 
-## TODO : define, validate lambda() --> mvn_ig_helper.R
-## test/validate lambda (closed form vs. numerical) 
+# testing: psi_mvn(), lambda_mvn_closed(), lambda_mvn()
 
-k = 1
+k = 2
 u = c(param_out[k,]$u1_star, param_out[k,]$u2_star, param_out[k,]$u3_star)
 
 exp(-psi_mvn(u, prior))     # (1 x 1) -- c_k[k] calculation
@@ -215,7 +212,18 @@ exp(-psi_mvn(u, prior))     # (1 x 1) -- c_k[k] calculation
 lambda_mvn_closed(u, prior) # (D x 1) -- lambda(u_star) calculation
 
 # check that numerical evaluation of gradient matches above closed form
-lambda_mvn(u, y, X, mu_beta, V_beta, a_0, b_0) # (D x 1)
+lambda_mvn(u, prior) # (D x 1)
+
+# check speed of both of these functions
+#microbenchmark(
+#    closed = lambda_mvn_closed(u, prior),
+#    numerical = lambda_mvn(u, prior),
+#    times = 50
+#)
+
+# closed form is about 120x faster
+
+
 
 ## (4) begin main algorithm 
 n_partitions = nrow(u_partition)
@@ -229,15 +237,14 @@ for (k in 1:n_partitions) {
     #                   param_out[k,]$u2_star,
     #                   y, m_0, w_0, r_0, s_0)) # (1 x 1)
     
-    # not ideal to do this here -- fix later (define another function)
-    u = c(param_out[k,]$u1_star, param_out[k,]$u2_star)
+    # TODO: avoid explicit definition of each representative point
+    u = c(param_out[k,]$u1_star, param_out[k,]$u2_star, param_out[k,]$u3_star)
     
-    c_k[k] = exp(-psi_rf(u, y, prior)) # (1 x 1)
+    c_k[k] = exp(-psi_mvn(u, prior)) # (1 x 1)
     
-    l_k = lambda_rf(u, y, prior)
+    l_k = lambda_mvn_closed(u, prior)
     
     integral_d = numeric(D) # store each component of the D-dim integral 
-    
     
     # nothing to refactor in this loop (i think?) since we're just iterating
     # thru each of the integrals and computing an exponential term
@@ -247,30 +254,170 @@ for (k in 1:n_partitions) {
         # changes (if columns get shuffled)
         
         # col id will change for D > 2
-        # TODO: generalize this better so there's less obscure calculation
-        col_id_lb = 5 + 2 * (d - 1)
+        # DONE: generalize this better so there's less obscure calculation
+        # col_id_lb = 5 + 2 * (d - 1)
+        # col_id_ub = col_id_lb + 1
+        
+        # updated 1/14: find column id of the first lower bound
+        col_id_lb = grep("u1_lb", names(param_out))
         col_id_ub = col_id_lb + 1
         
         # d-th integral computed in closed form
-        integral_d[d] = -1/l_k[d] * 
-            exp(-l_k[d] * (param_out[k,col_id_ub] - param_out[k,col_id_lb]))        
+        integral_d[d] = - 1 / l_k[d] * 
+            exp(- l_k[d] * (param_out[k, col_id_ub] - param_out[k, col_id_lb]))        
         
     }
     
     zhat[k] = prod(c_k[k], integral_d)
-    
 }
 
-log(sum(zhat))
+log(sum(-zhat))
+print(LIL_mvn_ig)
 
 
 # end of single approximation simulation ---------------------------------------
 
+## TODO : perform algorithm for a batch approximation (averaged) -- 
+##        essentially just pasting the code above into a loop
+
+approx_lil = function(N_approx, prior, post, D) {
+    
+    mu_star = post$mu_star
+    V_star  = post$V_star
+    a_n = post$a_n
+    b_n = post$b_n
+    
+    p = D - 1
+    
+    #### algorithm: main loop
+    N_iters = N_approx
+    
+    # test_out = numeric()
+    def_approx = numeric(N_iters)   # storage for default approximations (no r.p.)
+    
+    for (t in 1:N_iters) {
+        
+        if (t %% 10 == 0) {
+            print(paste("iter", t))
+        }
+        
+        # generate samples from the posterior probability to form the HME estimator
+        J = 3000 # number of random draws used per estimate
+        
+        # (0.1) sample from sigmasq | y
+        sigmasq_post = MCMCpack::rinvgamma(J, shape = a_n, scale = b_n) # (J x 1)
+        
+        # (0.2) sample from mu | sigmasq, y
+        beta_post = data.frame(matrix(0, J, p))                         # (J x p)
+        
+        for (j in 1:J) {
+            # each posterior sample is stored row-wise
+            beta_post[j,] = rmvnorm(1, mean = mu_star, sigma = sigmasq_post[j] * V_star)
+        }
+        
+        # store the posterior samples (row-wise) in a (J x D) matrix
+        # colnames: ( sigmasq_post, beta_post.X1, beta_post.X2, ... , beta_post.Xp )
+        # ** order of paramters matters, since helper functions assume a 
+        #    certain order when defined
+        u_post = data.frame(beta_post = beta_post, sigmasq_post = sigmasq_post)
+        
+        psi_u = apply(u_post, 1, psi_true_mvn, post = post) %>% unname() # (J x 1)
+        
+        # (1.2) construct u_df -- this will require some automation for colnames
+        u_df_names = character(D + 1)
+        for (d in 1:D) {
+            u_df_names[d] = paste("u", d, sep = '')
+        }
+        u_df_names[D + 1] = "psi_u"
+        
+        # populate u_df
+        u_df = cbind(u_post, psi_u) # J x (D + 1)
+        
+        # rename columns (needed since these are referenced explicitly in partition.R)
+        names(u_df) = u_df_names
+        
+        ## (2) fit the regression tree via rpart()
+        u_rpart = rpart(psi_u ~ ., u_df)
+        
+        
+        ## (3) process the fitted tree
+        
+        # (3.1) obtain the (data-defined) support for each of the parameters
+        param_support = matrix(NA, D, 2) # store the parameter supports row-wise
+        
+        for (d in 1:D) {
+            param_d_min = min(u_df[,d])
+            param_d_max = max(u_df[,d])
+            
+            param_support[d,] = c(param_d_min, param_d_max)
+        }
+        
+        # (3.2) obtain the partition --- moment of truth!!
+        u_partition = paramPartition(u_rpart, param_support)  # partition.R
+        
+        # organize all data into single data frame --> ready for approximation
+        param_out = u_star(u_rpart, u_df, u_partition, D)
+        
+        n_partitions = nrow(u_partition)
+        c_k = numeric(n_partitions)
+        zhat = numeric(n_partitions)
+        
+        for (k in 1:n_partitions) {
+            
+            # TODO: avoid explicit definition of each representative point
+            u = c(param_out[k,]$u1_star, param_out[k,]$u2_star, param_out[k,]$u3_star)
+            
+            c_k[k] = exp(-psi_mvn(u, prior)) # (1 x 1)
+            
+            l_k = lambda_mvn_closed(u, prior)
+            
+            integral_d = numeric(D) # store each component of the D-dim integral 
+            
+            # nothing to refactor in this loop (i think?) since we're just iterating
+            # thru each of the integrals and computing an exponential term
+            for (d in 1:D) {
+                # col id will change for D > 2
+                # DONE: generalize this better so there's less obscure calculation
+                # col_id_lb = 5 + 2 * (d - 1)
+                # col_id_ub = col_id_lb + 1
+                
+                # updated 1/14: find column id of the first lower bound
+                col_id_lb = grep("u1_lb", names(param_out))
+                col_id_ub = col_id_lb + 1
+                
+                # d-th integral computed in closed form
+                integral_d[d] = - 1 / l_k[d] * 
+                    exp(- l_k[d] * (param_out[k, col_id_ub] - param_out[k, col_id_lb]))        
+                
+            }
+            
+            zhat[k] = prod(c_k[k], integral_d)
+        }
+        
+        
+        def_approx[t] = log(sum(zhat))
+        
+        if (is.nan(def_approx[t])) {
+            def_approx[t] = log(-sum(zhat))
+        }
+        
+    }
+    
+    return(def_approx)
+    
+} # end of approx_lil()
 
 
+set.seed(1)
+N_approx = 10
+def_approx = approx_lil(N_approx, prior, post, D) # (100 x 1)
 
+def_approx
 
-
+# evaluate the mean, variance of the approximations
+mean(def_approx, na.rm = TRUE) # -106.1804
+var(def_approx, na.rm = TRUE)  # 4.491305
+print(LIL_mvn_ig)
 
 
 
