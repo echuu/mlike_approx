7af4bf60f7c8858be67de37cfacd6c8e76e16461
Eric Chuu <chuu3ric@gmail.com>
2020-02-13 13:00:53 -0600

    corrected AME code

--- hme/hme_estimator.R
+++ hme/hme_estimator.R
@@ -7,6 +7,7 @@ library('dplyr')
 library('ggplot2')
 library('MCMCpack') # for rinvgamma() function
 library('tree')     # plotting partitions of a fitted decision tree
+library('reshape2')
 
 # lenk paper simulation 
 
@@ -17,8 +18,8 @@ w_0 = 0.05
 r_0 = 3
 s_0 = 3
 
-N = 50
-
+N = 100
+set.seed(123)
 # generate 50 samples from N(mu, sigma_sq)
 y = rnorm(N, mu, sqrt(sigma_sq))
 
@@ -33,13 +34,13 @@ s_n = s_0 + sum((y - ybar)^2) + (N * w_0 / (N + w_0)) * (ybar - m_0)^2
 p_y = pi^(-N / 2) * (w_0 / w_n)^(1/2) * gamma(r_n / 2) / gamma(r_0 / 2) * 
     s_0^(r_0 / 2) / s_n^(r_n / 2)
 
-LIL = log(p_y) # -118.332 (paper says -117, but difference arises from RNG)
+LIL = log(p_y) # -214.5399 (paper says -117, but difference arises from RNG)
 
 
 # generate samples from the posterior probability to form the HME estimator
 
-J = 1000 # number of random draws used per estimate
-B = 1000 # number of batch estimators
+J = 40 # number of random draws used per estimate
+B = 20 # number of batch estimators
 
 
 # compute the estimate for the i-th batch
@@ -69,5 +70,102 @@ hme_df = data.frame(mcmc = 1:B, hme = log(lil_hat), lil = LIL)
 # generate figure 1 in lenk 2009
 ggplot(hme_df, aes(x = mcmc, y = hme)) + geom_point() +
     geom_hline(aes(yintercept = LIL), linetype = 'dashed', size = 0.9)
+
+
+# corrected arithmetic mean estimator ------------------------------------------
+
+
+# store log marginal likelihood estimators for B batches
+
+lil_came = numeric(B) # CAME estimator
+lil_hme  = numeric(B) # store the log integrated likelihood for each batch
+
+J = 40 # number of draws from the importance function
+B = 20 # number of batch estimators
+
+for (b in 1:B) {
+    
+    # (0) sample from mu | sigma_sq, y
+    mu_post = rnorm(J, m_n, sqrt(sigma_sq / w_n)) # (D x 1)
+    
+    # (1) sample from sigma_sq | y
+    sigma_sq_post = MCMCpack::rinvgamma(J, shape = r_n / 2, scale = s_n / 2)
+    
+    # (2) compute corrected arithmetic mean estimator  -------------------------
+    A_mu = c(min(mu_post), max(mu_post))
+    A_sigmasq = c(min(sigma_sq_post), max(sigma_sq_post))
+    
+    # draw from the importance density N-IG
+    mu_s      = rnorm(K, m_n, sqrt(sigma_sq / w_n))
+    sigmasq_s = MCMCpack::rinvgamma(K, shape = r_n / 2, scale = s_n / 2)
+    
+    # compute 1/s(theta) -- (K x 1) vector of evaluated densities
+    s_theta = dnorm(mu_s, m_n, sqrt(sigma_sq / w_n)) * 
+        MCMCpack::dinvgamma(sigmasq_s, shape = r_n / 2, scale = s_n / 2)
+    
+    # compute prior density
+    p_theta = dnorm(mu_s, m_0, sqrt(sigma_sq / w_0)) * 
+        MCMCpack::dinvgamma(sigmasq_s, shape = r_0 / 2, scale = s_0 / 2)
+    
+    # compute likelihood
+    lik_q = numeric(J)
+    for (q in 1:J) {
+        lik_q[q] = prod(dnorm(y, mean = mu_s[q], sd = sqrt(sigmasq_s[q])))
+    }
+    
+    ind_A = (mu_s >=  A_mu[1] & mu_s <= A_mu[2]) & 
+        (sigmasq_s >= A_sigmasq[1] & sigmasq_s <= A_sigmasq[2]) # 1_A (theta)
+    
+    lil_hat = 1 / s_theta * lik_q * p_theta * ind_A
+    # --------------------------------------------------------------------------
+    
+    # (2) compute the harmonic mean estimator
+    lik_j = numeric(J)
+    for (j in 1:J) {
+        # (2.1) compute the likelihood under the j-th posterior parameters
+        lik_j[j] = 1 /  prod(dnorm(y, mu_post[j], sqrt(sigma_sq_post[j])))
+    }
+    
+    # form the b-th HM estimator for the log integrated likelihood
+    lil_hme[b] = log(1 / (1 / J * sum(lik_j)))
+    lil_came[b] = log(mean(lil_hat))
+    
+}
+
+hme_df = data.frame(mcmc = 1:B, hme = lil_hme, came = lil_came, lil = LIL)
+
+hme_df_long = melt(hme_df, id.vars = "mcmc")
+
+ggplot(hme_df_long, aes(x = mcmc, y = value, col = variable)) + geom_point() +
+    geom_hline(aes(yintercept = LIL), linetype = 'dashed', size = 0.9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
