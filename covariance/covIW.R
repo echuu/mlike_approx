13547fe2d7d95287ce73bcd6c930b4067e9427f5
Eric Chuu <chuu3ric@gmail.com>
2020-04-02 22:41:27 -0700

    loglik, true logML, sampling all look good

--- covariance/covIW.R
+++ covariance/covIW.R
@@ -6,7 +6,7 @@ setwd(LEN_PATH)
 
 source("partition/partition.R")
 source("extractPartition.R")
-source("hybrid_approx.R")
+source("hybrid_approx_v1.R")
 source("covariance/covIW_helper.R")
 
 library(matrixcalc) # move into one of the other header files later
@@ -14,29 +14,33 @@ library(mvtnorm)
 
 ## define necessary parameters needed for this simulation
 
-J = 500                # num of MCMC samples (from true posterior in this case)
-N = 500                # number of observations
-p = 5                  # num rows/cols in the covariance matrix
-D = 0.5 * p * (p + 1)  # dimension of u that is fed into the tree
+J = 500                  # num of MCMC samples (from true posterior in this case)
+N = 500                  # number of observations
+D = 5                    # num rows/cols in the covariance matrix
+D_u = 0.5 * D * (D + 1)  # dimension of u that is fed into the tree
 
 
 ## wishart prior parameters
-Omega = diag(1, p)  # scale matrix
-nu    = p + 1       # degrees of freedom
+Omega = diag(1, D)  # scale matrix
+nu    = D + 1       # degrees of freedom
 
 ## specify the true covariance matrix
 set.seed(1)
-Sigma = matrix(rWishart(1, p, Omega), p)
+Sigma = matrix(rWishart(1, D, Omega), D)
 is.positive.definite(Sigma)
 
 ## generate data
-X = rmvnorm(N, mean = rep(0, p), sigma = Sigma) # (N x p)
-S = matrix(0, p, p)
+X = rmvnorm(N, mean = rep(0, D), sigma = Sigma) # (N x p)
+S = matrix(0, D, D)
 for (n in 1:N) {
     S = S + tcrossprod(X[n,]) # compute sum_n x_n * x_n'
 }
 
 # ------------------------------------------------------------------------------
+
+param_list = list(S = S, N = N, D = D, D_u = D_u, # S, dimension vars
+                  Omega = Omega, nu = nu,         # prior parameters
+                  u_df = NULL)                    # posterior samples
 
 ## TODO: obtain posterior samples
 Sigma_post = solve(Wishart_InvA_RNG(nu + N, S + Omega))
@@ -50,6 +54,65 @@ Sigma
 ## TODO: perform asymptotic analysis
 
 
+
+## verify that we have the correct expression for the true logML by plotting
+## logML vs. logN for 50 replicates per N
+
+J           = 300                               # num of MCMC samples from post
+K_sims      = 50                                # num of sims to run for each N
+N_vec_log   = seq(5, 12, by = 0.25)             # sample size grid unif in log
+N_vec       = floor(exp(N_vec_log)) %>% unique  # sample size to generate data
+LIL_N_k_hat = matrix(0, length(N_vec), K_sims)  # store approximations
+LIL_N       = numeric(length(N_vec))            # store true logML
+
+length(N_vec)
+
+
+for (i in 1:length(N_vec)) {
+    
+    N = N_vec[i]
+    
+    LIL_N_k = numeric(K_sims)
+    
+    for (k in 1:K_sims) {
+        
+        X = rmvnorm(N, mean = rep(0, D), sigma = Sigma) # (N x p)
+        S = matrix(0, D, D)
+        for (n in 1:N) {
+            S = S + tcrossprod(X[n,]) # compute sum_n x_n * x_n'
+        }
+        
+        param_list = list(S = S, N = N, D = D, D_u = D_u, # S, dimension vars
+                          Omega = Omega, nu = nu,         # prior parameters
+                          u_df = NULL)                    # posterior samples
+        LIL_N_k[k] = lil(param_list) - maxLogLik(Sigma, param_list)
+        
+    }
+    
+    LIL_N[i] = mean(LIL_N_k)
+    
+}
+
+
+LIL_df = data.frame(LIL_N = LIL_N, log_N = log(N_vec))
+# write.csv(LIL_df, "true_lil.csv", row.names = F) # N = seq(5, 12, by = 0.25)
+
+
+library(reshape2)
+library(ggpmisc)
+
+formula1 = y ~ x
+
+
+ggplot(LIL_df, aes(x = log_N, y = LIL_N)) + geom_point(size = 1.3) + 
+    geom_smooth(method = lm, se = F, formula = formula1) +
+    labs(x = "log(n)", y = "log(Z)", 
+         title = "Hybrid (Blue)") + 
+    stat_poly_eq(aes(label = paste(..eq.label.., sep = "~~~")), 
+                 label.x.npc = "right", label.y.npc = "top",
+                 eq.with.lhs = "logZ~`=`~",
+                 eq.x.rhs = "~logN",
+                 formula = formula1, parse = TRUE, size = 8)
 
 
 
--- covariance/covIW_helper.R
+++ covariance/covIW_helper.R
@@ -1,6 +1,15 @@
 
 ## covIW_helper.R
 
+source("C:/Users/ericc/Dropbox/eric chuu research/GGM/Q1.R")
+
+
+logmultigamma = function(p, a) {
+    f = 0.25 * p * (p - 1) * log(pi)
+    for(i in 1:p) { f = f + lgamma( a + 0.5 - 0.5 * i) }
+    return(f)
+} # end logmultigamma() function -----------------------------------------------
+
 
 preprocess = function(post_samps, D, params) {
     
@@ -20,11 +29,43 @@ preprocess = function(post_samps, D, params) {
     
     return(u_df)
     
-} # end of preprocess() function
+} # end of preprocess() function -----------------------------------------------
+
+lil = function(param_list) {
+    
+    N     = param_list$N      # number of observations
+    nu    = param_list$nu     # prior degrees of freedom for inv-wishart
+    D     = param_list$D      # num cols/rows in covariance matrix Sigma
+    Omega = param_list$Omega  # prior scale matrix for inv-wishart
+    S     = param_list$S      # sum_n x_n x_n'
+    
+    logML = logmultigamma(D, (nu + N) / 2) - 0.5 * N * D * log(pi) - 
+        logmultigamma(D, nu / 2) + 0.5 * nu * log_det(Omega) - 
+        0.5 * (N + nu) * log_det(Omega + S)
+    
+    return(logML)
+    
+} # end logML() function -------------------------------------------------------
+
+
+## TODO: maxLogLik() function
+maxLogLik = function(Sigma, param_list) {
+    
+    N     = param_list$N      # number of observations
+    D     = param_list$D      # num cols/rows in covariance matrix Sigma
+    S     = param_list$S      # sum_n x_n x_n'
+    
+    loglik = - 0.5 * N * D * log(2 * pi) - 0.5 * N * log_det(Sigma) -
+        0.5 * matrix.trace(solve(Sigma) %*% S)
+    
+    return(loglik)
+    
+} # end maxLogLik() function ---------------------------------------------------
 
 
 
-## TODO: loglik_true() function
+
+
 
 ## TODO: log_prior() function
 
