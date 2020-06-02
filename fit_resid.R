08f08b3b140af96c9e69b42371754e82f3eee295
Eric Chuu <chuu3ric@gmail.com>
2020-06-02 09:41:31 -0700

    resid fit partition function

--- fit_resid.R
+++ fit_resid.R
@@ -6,12 +6,15 @@
 # where psi_hat(u) is the optimal psi value chosen in the previous layer
 
 # params that will be passed into the function eventually
-n_samps = 10
+n_samps = 5
 
 # for the partition learned from prev fitted tree, extract the partition id and
 # the optimal value of psi for this partition
 prev_info = hml_approx$param_out %>% 
     dplyr::select(-c(psi_choice, logQ_cstar))
+
+
+prev_info = sub_part
 
 # prev_info = hml_approx$param_out %>% dplyr::select(leaf_id, psi_choice, n_obs)
 part_id = prev_info$leaf_id
@@ -19,10 +22,12 @@ part_id = prev_info$leaf_id
 K = length(part_id)
 k = 1
 
+
+set.seed(1)
 exp_terms = vector("list", K) 
 update_info = vector("list", K) 
-
 for (k in 1:K) {
+    
     print(k)
     N_k_p = prev_info$n_obs[k] * n_samps  # num of samples to draw from part k
     part_k = prev_info %>%                # set of lower/upper bounds
@@ -47,7 +52,7 @@ for (k in 1:K) {
     # optimal value of psi_star of the original k-th partition
     c_k_star = prev_info$psi_star[k] 
     
-    # TODO: compute R(u) for each of the samples; since we're in the k-th
+    # compute R(u) for each of the samples; since we're in the k-th
     # partition, we can directly subtract from psi_star[k], which we have
     # computed and stored from the previous (original) call to hml_approx()
     # this will need to be modified in the (to be)-implemented function that
@@ -55,55 +60,40 @@ for (k in 1:K) {
     R_df_k = u_df_k %>% dplyr::mutate(R_u = psi_u - c_k_star) %>% 
         dplyr::select(-psi_u)
     
-    # TODO: fit (u, R(u)) into decision tree to obtain partition
+    # fit (u, R(u)) into decision tree to obtain partition
     resid_tree = rpart(R_u ~ ., R_df_k)
     
-    # TODO: extract the (data-defined) support from R_df_k
+    # extract the (data-defined) support from R_df_k
     resid_support = extractSupport(R_df_k, D)
     
-    # TODO: extract partition from resid_tree
-    resid_partition = extractPartition(resid_tree, resid_support) 
-    
-    # TODO: for each of the partitions, obtain the optimal value as returned
-    # from the tree (can use previously defined objective function)
-    # this is stored in the psi_hat column of 'resid_partition'
+    # extract partition from resid_tree
+    # ntoe we omit 'psi_hat' the fitted value for the residual returned from 
+    # the tree, since this value results in an underestimation of the 
+    # log marginal likelihood. instead, use our own objective function to 
+    # choose the optimal value of R(u) for each (sub-)partition
+    resid_partition = extractPartition(resid_tree, resid_support) %>% 
+        dplyr::select(-psi_hat)
     
     # number of sub-partitions of partition k
     s_k = nrow(resid_partition) 
     
     # opt value (chosen by tree) for each sub-partition
-    # e_kj_star = resid_partition$psi_hat # (s_k x 1)-dim vector 
-    
-    e_kj_star = resid_partition %>% dplyr::select(leaf_id, psi_hat)
-    
-    # ss_partition[[k]] = c_k_approx
     e_kj_opt = partition_opt(resid_tree, R_df_k, resid_partition, D)
     
     resid_partition = e_kj_opt %>% dplyr::select(leaf_id, Ru_star) %>% 
         merge(resid_partition, by = 'leaf_id')
     
     # compute updated value for c_k_star to be used in final appro
-    # c_k_star_new = c_k_star + e_kj_star$psi_hat # TODO: replace w/ overwrite later
+
+    resid_partition = resid_partition %>% 
+        dplyr::mutate(psi_star = Ru_star + c_k_star) %>% 
+        dplyr::select(-Ru_star)
     
-    resid_partition = resid_partition %>% dplyr::rename(psi_star = psi_hat) %>% 
-        dplyr::mutate(psi_star = Ru_star + c_k_star)
+    # compute areas of A_k[j], j = 1, ... , s_k
+    # exp_terms[[k]] = compute_expterms(resid_partition, D)
     
-    # TODO: compute areas of A_k[j], j = 1, ... , s_k
-    # using only columns that are lb/ub, subtract lb from ub
-    resid_bounds = resid_partition %>% 
-        dplyr::select(-c(leaf_id, Ru_star, psi_star))
-    
-    part_len = resid_bounds[seq(2,ncol(resid_bounds),2)] - 
-        resid_bounds[seq(1,ncol(resid_bounds),2)]
-    log_len = log(part_len)
-    log_vol = rowSums(log_len) # log volume of each of the partitions
-    
-    # TODO: store residual partitions (might need later?)
-    
-    exp_terms[[k]] = -resid_partition$psi_star + log_vol
-    
-    # in order for the next recursive routine to be called, need:
-    # leaf_id, psi_star, lb/ub partitions, n_obs
+    # prepare for next recursive call by formatting the df to match the input: 
+    # leaf_id, lb/ub partitions, psi_star, n_obs
     part_tbl = table(resid_tree$where) %>% data.frame
     names(part_tbl) = c("leaf_id", "n_obs")
     
@@ -111,7 +101,7 @@ for (k in 1:K) {
 }
 
 
-log_sum_exp(unlist(exp_terms))
+# log_sum_exp(unlist(exp_terms))
 
 hml_approx$const_vec
 
@@ -119,27 +109,186 @@ hml_approx$const_vec
         log(TruncatedNormal::pmvnorm(mu_beta, Q_beta_inv, 
                                      lb = rep(0, D), ub = rep(Inf, D))[1]))
 
-
+# TODO: run next recursive call:
+# renumber the partition ids to make them unique
 sub_part = do.call(rbind, update_info)
+sub_part = sub_part %>% dplyr::mutate(leaf_id = 1:nrow(sub_part))
+
+# check that the number of sub-partitions matches the number of exp terms
+# length(unlist(exp_terms)) == nrow(sub_part)
+ss_approx = log_sum_exp(unlist(compute_expterms(sub_part, D)))
+ts_approx = log_sum_exp(unlist(compute_expterms(sub_part, D)))
+
+
 
 update_info[[1]]
 
 
-fit_resid = function(u_df_R, D) {
+## start here
+n_samps = 10
+
+# for the partition learned from prev fitted tree, extract the partition id and
+# the optimal value of psi for this partition
+prev_info = hml_approx$param_out %>% 
+    dplyr::select(-c(psi_choice, logQ_cstar))
+
+set.seed(1)
+sub_part = fit_resid(prev_info, D, n_samps, prior)
+
+log_sum_exp(unlist(compute_expterms(sub_part, D)))
+
+#### fit_resid() ---------------------------------------------------------------
+# curr_part : current partition: cols for leaf_id, psi_star, n_obs | ub/lbs
+# D         : dimension of parameter
+# n_samps   : multiplier for num (re)samples to draw from each partition
+fit_resid = function(curr_part, D, n_samps, params) {
     
-}
+    
+    part_id = curr_part$leaf_id        # extract partition IDs
+    K = length(part_id)                # number of partitions
+ 
+    # exp_terms = vector("list", K)      # store terms in the exp in final approx
+    sub_partitions = vector("list", K) # store the sub-partitions after resample
+    
+    for (k in 1:K) {
+        
+        print(k)
+        N_k_p = curr_part$n_obs[k] * n_samps  # num of samples to draw from A_k
+        
+        # set of lower/upper bounds corresponding to the k-th partition
+        # (1) omit the other three variables so that we only have ub/lb columns
+        # (2) transform row vector into a (D x 2) matrix of lb/ub
+        part_k = curr_part %>%                
+            dplyr::filter(leaf_id == part_id[k]) %>%
+            dplyr::select(-c(leaf_id, psi_star, n_obs)) %>% 
+            unlist() %>% matrix(ncol = 2, byrow = T)
+        
+        # sample unif from each lower/upper bound pair to form a D-dim vector
+        # lower bound in first column, upper bound in second column
+        # part_k_long = c(unlist(part_k)) %>% matrix(ncol = 2, byrow = T)
+        
+        # sample uniformly from the D-dim partition whose lower/upper bounds
+        # are defined in part_k above
+        resamp_k = Matrix_runif(N_k_p, 
+                                lower = part_k[,1], 
+                                upper = part_k[,2]) %>% data.frame
+        
+        # compute psi(u) for each of the samples
+        u_df_k = preprocess(resamp_k, D, params) # N_k_p x (D_u + 1)
+        
+        ## NEW CODE 5/29 -------------------------------------------------------
+        
+        # optimal value of psi_star of the original k-th partition
+        c_k_star = curr_part$psi_star[k] 
+        
+        # compute R(u) for each of the samples; since we're in the k-th
+        # partition, we can directly subtract from psi_star[k], which we have
+        # computed and stored from the previous (original) call to hml_approx()
+        # this will need to be modified in the (to be)-implemented func that
+        # handles fitting the residuals, rather than the func values of psi(u)
+        R_df_k = u_df_k %>% dplyr::mutate(R_u = psi_u - c_k_star) %>% 
+            dplyr::select(-psi_u)
+        
+        # fit (u, R(u)) into decision tree to obtain partition
+        resid_tree = rpart(R_u ~ ., R_df_k)
+        
+        # extract the (data-defined) support from R_df_k
+        resid_support = extractSupport(R_df_k, D)
+        
+        # extract partition from resid_tree
+        # ntoe we omit 'psi_hat' the fitted value for the residual returned from 
+        # the tree, since this value results in an underestimation of the 
+        # log marginal likelihood. instead, use our own objective function to 
+        # choose the optimal value of R(u) for each (sub-)partition
+        resid_partition = extractPartition(resid_tree, resid_support) %>% 
+            dplyr::select(-psi_hat)
+        
+        # number of sub-partitions of partition k
+        s_k = nrow(resid_partition) 
+        
+        # compute opt value (chosen by tree) for each sub-partition
+        # e_kj_opt : leaf_id, Ru_choice, Ru_star, logJ_star, n_obs, lb/ub
+        e_kj_opt = partition_opt(resid_tree, R_df_k, resid_partition, D)
+        
+        # extract only the optimal value (Ru_star) and partition id (leaf_id)
+        # and merge this back with the partition obtained from the fitted tree
+        resid_partition = e_kj_opt %>% dplyr::select(leaf_id, Ru_star) %>% 
+            merge(resid_partition, by = 'leaf_id')
+        
+        # compute updated value for c_k_star to be used in final approx
+        # update psi_star = c_k_star (prev value) + Ru_star (approx residual)
+        # resid_partition : leaf_id, psi_star, lb/ub
+        # (only piece of missing information now is the # of observations in 
+        # each partition)
+        resid_partition = resid_partition %>% 
+            dplyr::mutate(psi_star = Ru_star + c_k_star) %>% 
+            dplyr::select(-Ru_star)
+        
+        # compute areas of A_k[j], j = 1, ... , s_k
+        # exp_terms[[k]] = compute_expterms(resid_partition, D)
+        # prepare for next recursive call by formatting the df to match input: 
+        # leaf_id, lb/ub partitions, psi_star, n_obs
+        
+        # obtain number of observations in each partition
+        part_tbl = table(resid_tree$where) %>% data.frame
+        names(part_tbl) = c("leaf_id", "n_obs")
+        
+        # append column of number of observations to resid_partitions
+        sub_partitions[[k]] = merge(resid_partition, part_tbl, by = 'leaf_id')
+        
+    } # end of for loop iterating over partitions
+    
+    
+    # combine all sub-partitions into a dataframe containing all sub-partitions
+    sub_part_df = do.call(rbind, sub_partitions)
+    # redefine the partition IDs
+    sub_part_df = sub_part_df %>% dplyr::mutate(leaf_id = 1:nrow(sub_part_df))
+    
+    # note: sub_part matches the format of the input df, curr_part, and
+    # can be used to recursively/iteratively call this function
+    
+    return(sub_part_df)
+    
+} # ----------------------------------------------------------------------------
+
+
+
+
+
+## compute_expterms() ----------------------------------------------------------
+## part_info: df containing leaf_id, psi_star, lb/ub of D-intervals that make 
+## up each of the K partitions
+compute_expterms = function(part_info, D) {
+    
+    # exponential term for the k-th partition, A_k: 
+    # - psi_star_k + log(volume(A_k))
+    
+    # remove columns that are not lower/upper bounds
+    # bounds = part_info %>% dplyr::select(-c(leaf_id, psi_star))
+    
+    # uncomment below if we want to run this function on the final subpartition
+    # after exiting the loop rather than inside the loop
+    bounds = part_info %>% dplyr::select(-c(leaf_id, psi_star, n_obs))
+    
+    # (1) compute length of each of the D intervals for each partition
+    # (2) take log of each interval difference
+    # (3) add up each row for log volume of each partition
+    log_vol = (bounds[seq(2, 2 * D, 2)] - bounds[seq(1, 2 * D, 2)]) %>% 
+        log() %>% rowSums()
+    
+    return(-part_info$psi_star + log_vol) # K-dim vector
+} # end of compute_expterms() function -----------------------------------------
+
+
 
 
 # rpart_obj    : fitted rpart object
-# df           : D-dim posterior samps stored row-wise, function eval in last col
+# df           : D-dim posterior samps stored row-wise, func eval in last col
 # partition_df : partition id, lb/ub of each dimension
 # D            : dimension of parameter (posterior samples)
-
-
-df = R_df_k
-rpart_obj = resid_tree
-partition_df = resid_partition
-
+# df = R_df_k
+# rpart_obj = resid_tree
+# partition_df = resid_partition
 
 partition_opt = function(rpart_obj, df, partition_df, D) {
     
@@ -157,7 +306,18 @@ partition_opt = function(rpart_obj, df, partition_df, D) {
     
     ### start updated code 4/27
     Ru_df = df %>% dplyr::group_by(leaf_id) %>% 
-        summarise(psi_min  = min(R_u))
+        summarise(Ru_min  = min(R_u))
+    
+    
+    ## include more candidates Ru* to feed into objective function
+    Ru_quant = df %>% dplyr::group_by(leaf_id) %>% 
+        do(data.frame(t(quantile(.$R_u, probs = seq(0.01, 0.10, 0.01)))))
+    
+    names(Ru_quant) = c("leaf_id", paste('Ru_', seq(1, 10, 1), sep = ''))
+    
+    Ru_df = merge(Ru_df, Ru_quant, by = 'leaf_id') 
+    
+    # --------------------------------------------------------------------------
     
     Ru_long = melt(Ru_df, id.vars = c("leaf_id"), value.name = "Ru_star",
                     variable.name = "Ru_choice")
@@ -178,9 +338,19 @@ partition_opt = function(rpart_obj, df, partition_df, D) {
         
     } # end of loop extracting representative points
     
+    # select Ru* that minimizes objective function
+    Ru_all_df = Ru_all_df %>% 
+        group_by(leaf_id) %>% 
+        slice(which.min(logJ_star)) %>% 
+        data.frame()
+    
     ## merge with the boundary of each of the partitions
-    partition_star = merge(Ru_all_df, partition_df, by = 'leaf_id') %>% 
-        dplyr::select(-psi_hat)
+    # partition_star = merge(Ru_all_df, partition_df, by = 'leaf_id') %>%
+    #     dplyr::select(-Ru_hat)
+    
+    partition_star = merge(Ru_all_df, partition_df, by = 'leaf_id')
+    
+    # partition_star = merge(Ru_all_df, partition_df, by = 'leaf_id')
     
     # append the number of observations for each leaf node to the right
     # this is later used to determine the type of approximation to use
@@ -222,7 +392,7 @@ perc_thresh = sort(orig_partition$perc, decreasing = T)
 
 for (k in 1:K) {
     
-    N_k_p = part_0$n_obs[k] * n_samps  # number of (re)samples to draw from part k
+    N_k_p = part_0$n_obs[k] * n_samps  # num of (re)samples to draw from part k
     part_k = part_0 %>%           # set of lower/upper bounds
         dplyr::filter(leaf_id == part_set[k]) %>%
         dplyr::select(-c(leaf_id, n_obs))
--- truncate/regTN.R
+++ truncate/regTN.R
@@ -14,7 +14,7 @@ library(mvtnorm)
 setwd("C:/Users/ericc/mlike_approx/algo")
 source("setup.R")     
 source("C:/Users/ericc/mlike_approx/truncate/regTN_helper.R")
-D = 10
+D = 30
 N = 200
 I_D = diag(1, D)
 
